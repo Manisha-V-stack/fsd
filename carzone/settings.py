d397563b2e6f13b6dfbac4a1e28bdafdb6b508ae
Manisha-V-stack <vennapusamanisha168@gmail.com>
2026-03-03 20:57:14 +0530

    Switched to PostgreSQL

--- carzone/settings.py
+++ carzone/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 import cloudinary
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -60,11 +62,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'carzone.wsgi.application'
 
 # DATABASE (SQLite)
+# DATABASE (PostgreSQL from Render)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 # PASSWORD VALIDATION
