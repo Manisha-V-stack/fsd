f469c5b3003d59059582b18ca3324f704725538a
Manisha-V-stack <vennapusamanisha168@gmail.com>
2026-03-04 08:27:56 +0530

    Temporarily enable debug

--- carzone/settings.py
+++ carzone/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # For Render (you can restrict later)
 
